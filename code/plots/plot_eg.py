4107e810a7ed51a0a124897b1886cccaf0007eec
obi <ole.bialas@posteo.de>
2022-09-19 17:03:20 -0400

    [DATALAD] Recorded changes

--- code/plots/plot_eg.py
+++ code/plots/plot_eg.py
@@ -23,6 +23,55 @@ def convna(x):
         return float(x)
     return 
 
+fig, ax = plt.subplots(1, 3, sharey=True, sharex=True, figsize=(10,6))
+x = np.array([-50., -25.,   0.,  25.,  50.]) # plot all lines over this interval
+b_loc1, b_loc2, b_exp2 = [], [], []
+a_loc1, a_loc2, a_exp2 = [], [], []
+for subfolder in (root / "bids").glob("sub*"):
+    fname = subfolder / "beh" / f"{subfolder.name}_task-loctest_beh.tsv"
+    if fname.exists():
+        data = np.loadtxt(fname, skiprows=1, converters={0:convna, 1:convna})
+        data = data[~np.isnan(data[:, 1])]
+        b, a, _, _, _ = linregress(data[:, 0], data[:, 1])
+        if int(subfolder.name[-3:]) < 100:
+            a_loc1.append(a)
+            b_loc1.append(b)
+            ax[0].plot(x, line(x, a, b), color="gray", linewidth=0.5)
+        else:
+            a_loc2.append(a)
+            b_loc2.append(b)
+            ax[1].plot(x, line(x, a, b), color="gray", linewidth=0.5)
+
+    fname = subfolder / "beh" / f"{subfolder.name}_task-oneback_beh.tsv"
+    if fname.exists():
+        data = np.genfromtxt( fname, skip_header=1, usecols=(1, 2))
+        data = data[~np.isnan(data[:, 1])]
+        b, a, _, _, _ = linregress(data[:, 0], data[:, 1])
+        b_exp2.append(b)
+        a_exp2.append(a)
+        ax[2].plot(x, line(x, a, b), color="gray", linewidth=0.5)
+
+ax[0].plot(x, line(x, np.mean(a_loc1), np.mean(b_loc1)), color='red')
+ax[1].plot(x, line(x, np.mean(a_loc2), np.mean(b_loc2)), color='red')
+ax[2].plot(x, line(x, np.mean(a_exp2), np.mean(b_exp2)), color='red')
+
+ax[0].set(ylabel='Target Elevation [\u00b0]')
+ax[1].set(xlabel='Response Elevation [\u00b0]')
+
+
+for label, axes in zip(['A', 'B', 'C'], ax):
+    trans = mtransforms.ScaledTranslation(10/72, -5/72, fig.dpi_scale_trans)
+    axes.text(0.0, 1.0, label, transform=axes.transAxes + trans,
+            fontsize='medium', verticalalignment='top', fontfamily='serif')
+
+mean_eg = [np.mean(b_loc1), np.mean(b_loc2), np.mean(b_exp2)]
+
+for eg, axes in zip(mean_eg, ax):
+    label = r'$\overline{EG}$'+f'={eg.round(2)}'
+    trans = mtransforms.ScaledTranslation(10/72, -5/72, fig.dpi_scale_trans)
+    axes.text(0.6, 0.1, label, transform=axes.transAxes + trans,
+            fontsize='medium', verticalalignment='top', fontfamily='serif')
+
 fig, ax = plt.subplots(1, 3, sharey=True, sharex=True)
 x = np.array([-50., -25.,   0.,  25.,  50.]) # plot all lines over this interval
 b_loc1, b_loc2, b_exp2 = [], [], []
@@ -72,56 +121,7 @@ for eg, axes in zip(mean_eg, ax):
     axes.text(0.6, 0.1, label, transform=axes.transAxes + trans,
             fontsize='medium', verticalalignment='top', fontfamily='serif')
 
-
-fig, ax = plt.subplots(1, 3, sharey=True, sharex=True)
-x = np.array([-50., -25.,   0.,  25.,  50.]) # plot all lines over this interval
-b_loc1, b_loc2, b_exp2 = [], [], []
-a_loc1, a_loc2, a_exp2 = [], [], []
-for subfolder in (root / "bids").glob("sub*"):
-    fname = subfolder / "beh" / f"{subfolder.name}_task-loctest_beh.tsv"
-    if fname.exists():
-        data = np.loadtxt(fname, skiprows=1, converters={0:convna, 1:convna})
-        data = data[~np.isnan(data[:, 1])]
-        b, a, _, _, _ = linregress(data[:, 0], data[:, 1])
-        if int(subfolder.name[-3:]) < 100:
-            a_loc1.append(a)
-            b_loc1.append(b)
-            ax[0].plot(x, line(x, a, b), color="gray", linewidth=0.5)
-        else:
-            a_loc2.append(a)
-            b_loc2.append(b)
-            ax[1].plot(x, line(x, a, b), color="gray", linewidth=0.5)
-
-    fname = subfolder / "beh" / f"{subfolder.name}_task-oneback_beh.tsv"
-    if fname.exists():
-        data = np.genfromtxt( fname, skip_header=1, usecols=(1, 2))
-        data = data[~np.isnan(data[:, 1])]
-        b, a, _, _, _ = linregress(data[:, 0], data[:, 1])
-        b_exp2.append(b)
-        a_exp2.append(a)
-        ax[2].plot(x, line(x, a, b), color="gray", linewidth=0.5)
-
-ax[0].plot(x, line(x, np.mean(a_loc1), np.mean(b_loc1)), color='red')
-ax[1].plot(x, line(x, np.mean(a_loc2), np.mean(b_loc2)), color='red')
-ax[2].plot(x, line(x, np.mean(a_exp2), np.mean(b_exp2)), color='red')
-
-ax[0].set(ylabel='Target Elevation [\u00b0]')
-ax[1].set(xlabel='Response Elevation [\u00b0]')
-
-
-for label, axes in zip(['A', 'B', 'C'], ax):
-    trans = mtransforms.ScaledTranslation(10/72, -5/72, fig.dpi_scale_trans)
-    axes.text(0.0, 1.0, label, transform=axes.transAxes + trans,
-            fontsize='medium', verticalalignment='top', fontfamily='serif')
-
-mean_eg = [np.mean(b_loc1), np.mean(b_loc2), np.mean(b_exp2)]
-
-for eg, axes in zip(mean_eg, ax):
-    label = r'$\overline{EG}$'+f'={eg.round(2)}'
-    trans = mtransforms.ScaledTranslation(10/72, -5/72, fig.dpi_scale_trans)
-    axes.text(0.6, 0.1, label, transform=axes.transAxes + trans,
-            fontsize='medium', verticalalignment='top', fontfamily='serif')
-
+[a.set(adjustable='box', aspect='equal') for a in ax]
 
 plt.savefig(root/'paper'/'figures'/'eg.png', dpi=800)
 
