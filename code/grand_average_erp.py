c95d653ac469b577377083e64124adbd84f13a24
obi <ole.bialas@posteo.de>
2022-09-30 15:24:27 -0400

    [DATALAD] Recorded changes

---
+++ code/grand_average_erp.py
@@ -0,0 +1,23 @@
+"""
+Compute a linear tuning curve for each subject
+"""
+import re
+from pathlib import Path
+from mne import grand_average, read_epochs
+
+root = Path(__file__).parent.parent.absolute()
+
+evokeds1, evokeds2 = [], []
+for subfolder in (root / "preprocessed").glob("sub-*"):
+    epochs = read_epochs(subfolder / f"{subfolder.name}-epo.fif")
+    if int(re.search(r"\d+", subfolder.name).group()) < 100:
+        evokeds1.append(epochs.average())
+    else:
+        evokeds2.append(epochs.average())
+evoked1 = grand_average(evokeds1)
+evoked2 = grand_average(evokeds2)
+evoked2.info["bads"].append("F1")
+evoked2.interpolate_bads()
+
+evokeds1.save(root / "results" / "grand_averageI-evo.fif")
+evokeds2.save(root / "results" / "grand_averageII-evo.fif")
