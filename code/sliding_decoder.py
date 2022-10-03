0620ba1363926129fefbce27d2226d6a8b268760
obi <ole.bialas@posteo.de>
2022-10-03 15:29:19 -0400

    [DATALAD] Recorded changes

--- code/sliding_decoder.py
+++ code/sliding_decoder.py
@@ -1,43 +1,38 @@
-import sys
 from pathlib import Path
 import itertools
 import numpy as np
-import argparse
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from mne import read_epochs
 from mne.decoding import SlidingEstimator, cross_val_multiscore
+
 root = Path(__file__).parent.parent.absolute()
-sys.path.append(str(root/"code"))
-from utils import get_epochs, get_transforms, apply_transform
+n_crossval = 100
 
-parser = argparse.ArgumentParser()
-parser.add_argument('experiment', type=str, choices=["freefield", "headphones"])
-parser.add_argument('subject', type=str)
-parser.add_argument('-n', '--n_components', type=int, default=3)
-parser.add_argument('-cv', '--cross_val', type=int, default=10)
-parser.add_argument('--n_jobs', type=int, default=1)
-args = parser.parse_args()
-
-epochs = get_epochs(args.subject)
-epochs.resample(100)
-# to_jd1, from_jd1, to_jd2, from_jd2 = get_transforms(args.subject)
-# components = apply_transform(apply_transform(epochs.get_data(), to_jd1), to_jd2[:, 0:args.n_components])
-components = epochs._data
-events, event_id = epochs.events, epochs.event_id
-n_times = len(epochs.times)
-del epochs
-combinations = [",".join(map(str, comb)) for comb in itertools.combinations(event_id.keys(), 2)]
-combinations = [comb.split(",") for comb in combinations]
-
-scores = np.zeros((len(combinations), args.cross_val, n_times))
-for i, (con1, con2) in enumerate(combinations):
-    mask = np.logical_or(events[:, 2] == event_id[con1], events[:, 2] == event_id[con2])
-    X = components[mask]
-    y = events[mask][:, 2]
-    clf = make_pipeline(StandardScaler(), LogisticRegression(solver='lbfgs'))
-    decoder = SlidingEstimator(clf, n_jobs=args.n_jobs, scoring='roc_auc', verbose=True)
-    scores[i, :, :] = cross_val_multiscore(decoder, X, y, cv=args.cross_val, n_jobs=args.n_jobs)
-
-np.save(root/args.experiment/"output"/args.subject/f"combinations.npy", np.array(combinations))
-np.save(root/args.experiment/"output"/args.subject/f"accuracy.npy", scores)
+for subfolder in (root / "preprocessed").glob("sub-1*"):
+    epochs = read_epochs(subfolder / f"{subfolder.name}-epo.fif")
+    events, event_id = epochs.events, epochs.event_id
+    n_times = len(epochs.times)
+    combinations = [
+        ",".join(map(str, c)) for c in itertools.combinations(epochs.event_id.keys(), 2)
+    ]
+    results = {}
+    combinations = [comb.split(",") for comb in combinations]
+    scores = np.zeros((len(combinations), n_crossval, n_times))
+    for i, (con1, con2) in enumerate(combinations):
+        mask = np.logical_or(
+            events[:, 2] == event_id[con1], events[:, 2] == event_id[con2]
+        )
+        X = epochs.get_data()[mask]
+        y = events[mask][:, 2]
+        clf = make_pipeline(
+            StandardScaler(), LogisticRegression(solver="lbfgs", max_iter=1000)
+        )
+        decoder = SlidingEstimator(clf, scoring="roc_auc", verbose=True)
+        scores = cross_val_multiscore(decoder, X, y, cv=n_crossval)
+        results[f"{con1} vs {con2}"] = scores
+        np.save(
+            root / "results" / subfolder.name / f"{subfolder.name}-decoding.npy",
+            results,
+        )
