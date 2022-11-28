53bed48fb6104899fc67ed886493b8cf462f6c7a
obi <ole.bialas@posteo.de>
2022-11-28 14:56:53 -0500

    balance conditions for cluster test

--- code/cluster_test.py
+++ code/cluster_test.py
@@ -12,6 +12,8 @@ n_permutations = 10000
 def run_cluster_test(epochs, event_ids):
     """Run a cluster test on `epochs` comparing the conditions in `event_ids`."""
     # get the trials corresponding to the different stimuli
+    conditions = list(epochs.event_id.keys())[:-1]
+    epochs.equalize_event_counts(conditions)
     adjacency = find_ch_adjacency(epochs.info, "eeg")[0]
     indices = [np.where(epochs.events[:, 2] == event_id)[0] for event_id in event_ids]
     data = [epochs.get_data()[idx, :, :].transpose(0, 2, 1) for idx in indices]
@@ -23,6 +25,8 @@ def run_cluster_test(epochs, event_ids):
 
 for subfolder in (root / "preprocessed").glob("sub-*"):
     epochs = read_epochs(subfolder / f"{subfolder.name}-epo.fif")
+    if int(subfolder.name[-3:]) < 100:
+        epochs.crop(None, 1.0)
     outfolder = root / "results" / subfolder.name
     if not outfolder.exists():
         outfolder.mkdir()
