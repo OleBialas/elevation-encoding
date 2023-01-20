f561414e40d971fc9e770bd725caca57def8b314
obi <ole.bialas@posteo.de>
2023-01-20 15:06:19 -0500

    plot details

--- code/plots/plot_decoding.py
+++ code/plots/plot_decoding.py
@@ -3,8 +3,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy.signal import savgol_filter
-from scipy.stats import linregress
-from mne import read_evokeds
+from scipy.stats import linregress, binom
+from mne import read_epochs
+
+root = Path(__file__).parent.parent.parent
+plt.style.use(["science", "no-latex"])
+colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 
 
 def convna(x):
@@ -19,19 +23,18 @@ def line(x, a, b):
     return a + x * b
 
 
-root = Path(__file__).parent.parent.parent
-plt.style.use(["science", "no-latex"])
-colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
-
 fig, ax = plt.subplot_mosaic([["A", "B"]], figsize=(8, 4))
 divider = make_axes_locatable(ax["B"])
 ax["C"] = divider.append_axes("top", size="20%", pad=0)
 ax["D"] = divider.append_axes("right", size="20%", pad=0)
 
-evoked = read_evokeds(root / "results" / "grand_averageII-ave.fif")[0]
+times = read_epochs(root / "preprocessed" / "sub-100" / "sub-100-epo.fif").times
 n_permute = 10000
 tmin, tmax = 0.15, 0.9
 adapter_dur = 1.0
+n_obs = 360
+thresh = binom.ppf(0.95, n, 0.5) / n  # threshold for p < .05 for one-sided test
+
 plt.style.use(["science", "no-latex"])
 subjects = list((root / "results").glob("sub-1*"))
 # calculate average decoding accros cross validation folds per subject
@@ -45,9 +48,6 @@ for isub, sub in enumerate(subjects):
 
 # get each subjects elevation gain
 eg_test, eg_task = [], []  # elevation gain
-
-print(f"localization test: \n {linregress(eg_test, avg_acc)}")
-print(f"localization task: \n {linregress(eg_task, avg_acc)}")
 
 for subfolder in (root / "bids").glob("sub-1*"):
     fname = subfolder / "beh" / f"{subfolder.name}_task-loctest_beh.tsv"
@@ -66,8 +66,8 @@ eg_task = np.asarray(eg_task)
 eg_task[eg_task < 0] = 0  # set negative EGs to 0
 
 # get each subjects mean decoding accuracy between tmin and tmax
-nmin = np.argmin(np.abs(adapter_dur + tmin - evoked.times))
-nmax = np.argmin(np.abs(adapter_dur + tmax - evoked.times))
+nmin = np.argmin(np.abs(adapter_dur + tmin - times))
+nmax = np.argmin(np.abs(adapter_dur + tmax - times))
 avg_acc = decoding_data[:, :, nmin:nmax].mean(axis=(1, 2))
 
 # calculate regression between eg and acc with bootstrapping
@@ -84,22 +84,27 @@ for ip in range(n_permute):
 # plot the data
 for ikey, key in enumerate(keys):
     ax["A"].plot(
-        evoked.times - adapter_dur,
-        savgol_filter(decoding_data.mean(axis=0)[ikey], 13, 5),
+        times - adapter_dur,
+        savgol_filter(decoding_data.mean(axis=0)[ikey], 50, 8),
         label=key,
     )
 ax["A"].legend(loc="upper left")
 ax["A"].set(
-    xlabel="Time [s]", ylabel="Accuracy [a.u.c.]", yticks=[0.5, 0.55, 0.6, 0.65]
+    xlabel="Time [s]",
+    ylabel="Accuracy [a.u.c.]",
+    yticks=[0.5, 0.55, 0.6, 0.65],
+    xlim=(-1, 1.2),
 )
+ax["A"].axhline(y=thresh, xmin=0, xmax=1, color="black", linestyle="--")
 
+labels = ["Test", "Task"]
 for i, data in enumerate([resampled_test, resampled_task]):
     if i == 0:
         eg = eg_test
     else:
         eg = eg_task
     mean, std = data.mean(axis=0), data.std(axis=0)
-    ax["B"].scatter(eg, avg_acc, color=colors[i])
+    ax["B"].scatter(eg, avg_acc, color=colors[i], label=labels[i])
     ax["B"].plot(x, mean, color=colors[i])
     ax["B"].fill_between(x, mean + 2 * std, mean - 2 * std, alpha=0.2, color=colors[i])
     ax["B"].set(
@@ -110,6 +115,7 @@ for i, data in enumerate([resampled_test, resampled_task]):
         yticks=[0.5, 0.55, 0.6, 0.65, 0.7],
     )
 
+ax["B"].legend()
 ax["B"].text(0.15, 0.56, "**")
 ax["B"].text(0.22, 0.48, "n.s.")
 ax["C"].hist(eg_test, alpha=0.5, bins=20, color=colors[0])
@@ -121,6 +127,24 @@ ax["D"].set(ylim=(0.45, 0.75), yticks=[], xticks=[2])
 plt.tight_layout()
 fig.text(0.95, 0.85, "number\n of\n subjects", ha="center")
 
-fig.text(0.05, 0.93, "A", size=10, weight="bold")
-fig.text(0.54, 0.93, "B", size=10, weight="bold")
+
+ax["B"].annotate(
+    "**",
+    xy=(0.65, resampled_task.mean(axis=0)[65]),
+    xycoords="data",
+    xytext=(-50, 20),
+    textcoords="offset points",
+    arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=-.2"),
+)
+
+ax["B"].annotate(
+    "n.s.",
+    xy=(0.35, resampled_test.mean(axis=0)[35]),
+    xycoords="data",
+    xytext=(40, -40),
+    textcoords="offset points",
+    arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=-.2"),
+)
+
+
 fig.savefig(root / "results" / "plots" / "decoding.png", dpi=300, bbox_inches="tight")
